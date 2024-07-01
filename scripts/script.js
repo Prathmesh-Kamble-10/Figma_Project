3bbc33b91a0257ba26ab6e52e6f62eb38a70d030
Prathmesh-Kamble-10 <prathmeshkamble885@gmail.com>
2024-07-02 00:04:58 +0530

    profile css and script.js file is updated

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function dataTransfer(){
+    document.querySelector("#school").innerText=document.getElementById("schoolName").value;
+    document.getElementsByClassName("img").src=document.getElementById("img").value;
+    document.getElementById("studentName_val").innerText=document.getElementById("studentName").value;
+    document.getElementById("DateOfBirth_val").innerText=document.getElementById("DateOfBirth").value;
+    document.getElementById("MobileNov_al").innerText=document.getElementById("MobileNo").value;
+    document.getElementById("RollNo_val").innerText=document.getElementById("RollNo").value;
+    document.getElementById("Address_val").innerText=document.getElementById("Address").value;
+}
